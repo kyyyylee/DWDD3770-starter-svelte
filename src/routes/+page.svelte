6c2f0d59018a1bbf2c14035f5da62b3512e6c373
Kylee Harward <kyleeharward21@gmail.com>
2023-09-13 18:34:35 -0600

    got css working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,11 @@
 	let name = 'Svelte'
 </script>
 
-<h1>Welcome to my {name} page!</h1>
+<h1 class="text-yellow-400 font-bold">Welcome to my {name} page!</h1>
+
+<!-- <style>
+	h1 {
+		color:goldenrod;
+		font-family: 'Courier New', 'Serif';
+	}
+</style> -->
