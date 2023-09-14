786149dc51ca9b88fe469cf61b3ca2124a44daf3
Kylee Harward <kyleeharward21@gmail.com>
2023-09-13 18:39:29 -0600

    made nested comonent

---
+++ src/lib/components/Nested.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let message: string = 'Hello from the nested file';
+</script>
+
+<div class=" bg-pink-200 m-6 text-center py-6">
+	<p>{@html message}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang='ts'>
 	let name = 'Svelte'
+	import Nested from '$lib/components/Nested.svelte';
+	import nested from '$lib/components/Nested.svelte'
 </script>
 
 <h1 class="text-yellow-400 font-bold">Welcome to my {name} page!</h1>
-
+<Nested />
 <!-- <style>
 	h1 {
 		color:goldenrod;
