521764ca06f3c73a8d4c523d8d1e4238b11c7f78
Kylee Harward <kyleeharward21@gmail.com>
2023-09-20 14:05:43 -0600

    added link to tutorials from home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<h1>This is my super awesome HOME PAGE ROUTE</h1>
+<h1 class="text-center m-4">This is my super awesome HOME PAGE ROUTE</h1>
+<div class="flex justify-center">
+    <button class="btn btn-lg rounded-full bg-pink-500"><a href="/tutorial">Go to tutorials</a></button>
+</div>
