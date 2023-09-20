d12bb1394a2d081a3e2911385b62734dd9282136
Kylee Harward <kyleeharward21@gmail.com>
2023-09-20 11:54:43 -0600

    fixed configuration

--- src/routes/tutorial/introduction/+page.svelte
+++ src/routes/tutorial/introduction/+page.svelte
@@ -1,7 +1,6 @@
 <script lang='ts'>
 	let name = 'Svelte'
 	import Nested from '$lib/components/Nested.svelte';
-	import nested from '$lib/components/Nested.svelte'
 </script>
 
 <h1 class="text-yellow-400 font-bold">Welcome to my {name} page!</h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
 			themes: {
-				preset: [{ name: 'wintry', enhancements: true }]
+				preset: [{ name: 'gold-nouveau', enhancements: true }]
 			}
 		})
 	]
