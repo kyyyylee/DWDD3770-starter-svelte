1c8117c8b652893c2e84140ac96fee4e1b23a598
Kylee Harward <kyleeharward21@gmail.com>
2023-09-20 15:19:08 -0600

    added a navigation for tutorials

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -1,3 +1,9 @@
 <div class="text-pink-200">
-    <h2>Placeholder for my tutorial navbar</h2>
+    <h2 class="m-4 text-center">Placeholder for my tutorial navbar</h2>
+    <div class="flex justify-center p-4">
+        <button class="btn btn-lg rounded-full bg-pink-500 m-2"><a href="/tutorial/introduction">Introduction</a></button>
+        <button class="btn btn-lg rounded-full bg-pink-500 m-2"><a href="/tutorial/reactivity">Reactivity</a></button>
+        <button class="btn btn-lg rounded-full bg-pink-500 m-2"><a href="/tutorial/props">Props</a></button>
+        <button class="btn btn-lg rounded-full bg-pink-500 m-2"><a href="/tutorial/logic">Logic</a></button>
+    </div>
 </div>
