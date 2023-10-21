9c47e888595fb320d3ad2d9f3fb0f39aae10035a
Kylee Harward <kyleeharward21@gmail.com>
2023-10-20 19:38:43 -0600

    added super awesome spotify widget

--- src/routes/tutorial/lifecycle/+page.svelte
+++ src/routes/tutorial/lifecycle/+page.svelte
@@ -5,6 +5,7 @@
 	let songName: string;
     let songLyric: string;
 	let albumCoverSrc: string;
+	let iframeSrc: string;
     /***/
 
 	onMount(async () => {
@@ -19,33 +20,44 @@
 
 		if (song.album_id === 1) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/d/d5/Taylor_Swift_-_1989_%28Taylor%27s_Version%29.png"
+			iframeSrc = "https://open.spotify.com/embed/album/34OkZVpuzBa9y40DCy0LPR?utm_source=generator"
 		}
 		if (song.album_id === 2) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/1/1f/Taylor_Swift_-_Taylor_Swift.png"
+			iframeSrc = "https://open.spotify.com/embed/album/7mzrIsaAjnXihW3InKjlC3?utm_source=generator"
 		}
 		if (song.album_id === 3) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/5/5b/Fearless_%28Taylor%27s_Version%29_%282021_album_cover%29_by_Taylor_Swift.png"
+			iframeSrc = "https://open.spotify.com/embed/album/4hDok0OAJd57SGIT8xuWJH?utm_source=generator"
 		}
 		if (song.album_id === 4) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/5/5b/Taylor_Swift_-_Speak_Now_%28Taylor%27s_Version%29.png"
+			iframeSrc = "https://open.spotify.com/embed/album/5AEDGbliTTfjOB8TSm1sxt?utm_source=generator"
 		}
 		if (song.album_id === 5) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/4/47/Taylor_Swift_-_Red_%28Taylor%27s_Version%29.png"
+			iframeSrc = "https://open.spotify.com/embed/album/6kZ42qRrzov54LcAk4onW9?utm_source=generator"
 		}
 		if (song.album_id === 6) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/f/f2/Taylor_Swift_-_Reputation.png"
+			iframeSrc = "https://open.spotify.com/embed/album/6DEjYFkNZh67HP7R9PSZvv?utm_source=generator"
 		}
 		if (song.album_id === 7) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/c/cd/Taylor_Swift_-_Lover.png"
+			iframeSrc = "https://open.spotify.com/embed/album/1NAmidJlEaVgA3MpcPFYGq?utm_source=generator"
 		}
 		if (song.album_id === 8) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/f/f8/Taylor_Swift_-_Folklore.png"
+			iframeSrc = "https://open.spotify.com/embed/album/1pzvBxYgT6OVwJLtHkrdQK?utm_source=generator"
+
 		}
 		if (song.album_id === 9) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/0/0a/Taylor_Swift_-_Evermore.png"
+			iframeSrc = "https://open.spotify.com/embed/album/6AORtDjduMM3bupSWzbTSG?utm_source=generator"
 		}
 		if (song.album_id === 10) {
 			albumCoverSrc = "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnights_-_Taylor_Swift.png"
+			iframeSrc = "https://open.spotify.com/embed/album/3lS1y25WAhcqJDATJK70Mq?utm_source=generator"
 		}
 
 		console.log(song.album_id);
@@ -91,19 +103,30 @@
 	{:else}
 		<p>...</p>
 	{/if}
-	
+
 	{#if songLyric}
 	<h2 class="mt-4 mb-2">Lyrics:</h2>
 	<textarea
-		class="textarea text-center mx-auto w-2/3 h-64"
+		class="textarea text-center mx-auto w-2/3 h-64 max-w-lg"
 		value={songLyric}
 		on:keydown={handleKeydown}
 	/>
-    <p class="p-2 text-surface-700">Hint: Hit 'tab' on your favorite lyrics to make them uppercase!</p>
+    <p class="p-2 mb-2 text-surface-700">Hint: Hit 'tab' on your favorite lyrics to make them uppercase!</p>
 	{:else}
 		<p>...</p>
 	{/if}
+
+<iframe class="max-w-3xl"
+style="border-radius:12px"
+title="Taylor Swift Music Video"
+src={iframeSrc}
+width="70%"
+height="352"
+frameBorder="0"
+allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+loading="lazy"
+></iframe>
 	 
 <!-- 	<button on:click={refreshPage}>Click for new song!</button>
  --></div>
-<p class="text-center clear-both bottom-0 mt-8 p-2">created using <a href="https://github.com/sarbor/taylor_swift_api" target="_blank">Taylor Swift API</a></p>
+<p class="text-center clear-both bg-surface-100 border-t-2 border-b-2 border-surface-500 bottom-0 mt-8 p-2">created using <a class="font-bold" href="https://github.com/sarbor/taylor_swift_api" target="_blank">Taylor Swift API</a></p>
