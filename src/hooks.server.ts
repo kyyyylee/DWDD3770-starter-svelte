905969739367d040e933d4c65d24bffec8a4954d
Kylee Harward <kyleeharward21@gmail.com>
2023-11-03 14:20:51 -0600

    added env variables to vercel

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,8 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import Google from "@auth/core/providers/google"
-import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
+import { GITHUB_ID, GITHUB_SECRET} from "$env/static/private"
 
 export const handle = SvelteKitAuth({
   providers: [
-    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
-    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
-  ],
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),  ],
 })
